e652b8d003d58616798d8be5f2b986ebac226c2e
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2024-11-12 14:14:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
-            
+            var menuItems = new List<MenuItem>
+            {
+                new MenuItem(1, "packas", 15.50M),
+                new MenuItem(2, "TE", 1.50M),
+            };
+            var restaurant = new Restaurant(menuItems);
+
+            var order = new Order(menuItems.FindAll(item => item.Id == 1), 1);
+            restaurant.CreateOrder(order);
         }
     }
 }
